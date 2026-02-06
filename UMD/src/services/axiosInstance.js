c91d7ccd640419a61d1161d9802749b5687fe269
Abirami <abirami.r@bluenett.com>
2026-02-06 18:15:47 +0530

    first commit

---
+++ UMD/src/services/axiosInstance.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL:"https://reqres.in/api",
+    headers:{
+        "x-api-key":import.meta.env.VITE_REQRES_API_KEY
+    }
+}
+)
+    export default api
